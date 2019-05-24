c1081fdf31ddc329dc8eb071031d5c905592aade
Andrii Maryskevych <andrii.maryskecych@gmail.com>
2019-05-24 20:55:55 +0300

    Cleaning up

--- src/components/app.js
+++ src/components/app.js
@@ -1,23 +1,11 @@
-import React, { Component, Fragment } from 'react';
-import { Route, Switch, Redirect, Link } from "react-router-dom";
-
+import React, { Component } from 'react';
 import FEM from './FEM';
-import Home from './Home';
 
 export default class App extends Component {
     render() {
         return (
             <div className='container-fluid'>
-                <div className='navbar navbar-left'>
-                    <Link to="/">Home</Link>
-                    <Link to="/fem">FEM</Link>
-                </div>
-
-                <Switch>
-                    <Route exact path="/fem" component={FEM}/>
-                    <Route exact path="/" component={Home}/>
-                    <Redirect from="*" to="" />
-                </Switch>
+                <FEM />
             </div>
         );
     }
