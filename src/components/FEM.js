5f2e490035ec44bb3833ecff7fc39d629a57cf3b
Andrii Maryskevych <andrii.maryskecych@gmail.com>
2019-05-25 09:10:49 +0300

    Removed redundand socket emition

--- src/components/FEM.js
+++ src/components/FEM.js
@@ -15,7 +15,6 @@ export default class FEM extends Component {
     }
 
     componentDidMount() {
-        this.state.socket.emit('start');
         threeEntryPoint(this.threeRootElement, this.state.socket);
     }
 
