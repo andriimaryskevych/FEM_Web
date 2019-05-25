5b30b47d92f636c36cdf3f6e4b4b00cd51164827
Andrii Maryskevych <andrii.maryskecych@gmail.com>
2019-05-25 11:05:15 +0300

    Configure scene

--- src/services/canvas-drawer.js
+++ src/services/canvas-drawer.js
@@ -3,92 +3,63 @@ window.THREE = THREE;
 require('three/examples/js/controls/OrbitControls');
 
 class CanvasDrawer {
-    constructor (canvas, socket) {
+    constructor(canvas, socket) {
         const width = canvas.offsetWidth;
         const height = canvas.offsetHeight;
+
         canvas.setAttribute('width', width);
         canvas.setAttribute('height', height);
 
-        let renderer = new THREE.WebGLRenderer({canvas: canvas});
-        renderer.setClearColor(0xffffff);
+        var scene = new THREE.Scene();
+        scene.background = new THREE.Color(0xf0f0f0);
 
-        let scene = new THREE.Scene();
-
-        let camera = new THREE.PerspectiveCamera(45, width / height, 0.1, 5000);
-        camera.position.set(0, 0, 100);
+        var camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 1, 10000);
+        camera.position.set(100, 100, 100);
         camera.lookAt( new THREE.Vector3( 50, 50, 50 ) );
+        scene.add(camera);
+        scene.add(new THREE.AmbientLight(0xf0f0f0));
 
-        let light = new THREE.AmbientLight(0xffffff);
+        var light = new THREE.SpotLight(0xffffff, 1.5);
+        light.position.set(0, 1500, 200);
+        light.castShadow = true;
+        light.shadow = new THREE.LightShadow(new THREE.PerspectiveCamera(70, 1, 200, 2000));
+        light.shadow.bias = -0.000222;
+        light.shadow.mapSize.width = 1024;
+        light.shadow.mapSize.height = 1024;
         scene.add(light);
 
-        var loader = new THREE.FileLoader();
+        var planeGeometry = new THREE.PlaneBufferGeometry(2000, 2000);
+        planeGeometry.rotateX(-Math.PI / 2);
 
-        // adding red points that represent statr poistion
-        let startPoints;
-        let start;
+        var planeMaterial = new THREE.ShadowMaterial({ opacity: 0.2 });
 
-        socket.on('start.txt', (data) => {
-            if (start) {
-                scene.remove(start);
-                start.geometry.dispose();
-                start.material.dispose();
-                start = undefined;
+        var plane = new THREE.Mesh(planeGeometry, planeMaterial);
+        plane.position.y = -200;
+        plane.receiveShadow = true;
+        scene.add(plane);
 
-                scene.remove(result);
-                result.geometry.dispose();
-                result.material.dispose();
-                result = undefined;
-            }
+        var helper = new THREE.GridHelper(2000, 100);
+        helper.material.opacity = 0.25;
+        helper.material.transparent = true;
+        scene.add(helper);
 
-            startPoints = JSON.parse(data);
+        var axes = new THREE.AxesHelper(1000);
+        axes.position.set(0, 0, 0);
+        scene.add(axes);
 
-            var starsMaterial = new THREE.PointsMaterial( { color: 'black', size: 2.0 } );
-            var geometry = new THREE.Geometry();
+        let renderer = new THREE.WebGLRenderer({ canvas });
+        renderer.setPixelRatio(window.devicePixelRatio);
+        renderer.setClearColor(0xffffff);
 
-            for(var i = 0; i < startPoints.length; i++)
-            {
-                geometry.vertices.push(new THREE.Vector3( startPoints[i].x, startPoints[i].y, startPoints[i].z) );
-            }
+        let controls = new THREE.OrbitControls(camera, canvas);
+        controls.addEventListener('change', render);
+        controls.update();
 
-            start = new THREE.Points( geometry, starsMaterial );
-
-            scene.add(start);
-        });
-
-        // adding green points that represent result poistion
-        let resultPoints;
-        let result;
-
-        socket.on('points.txt', (data) => {
-            resultPoints = JSON.parse(data);
-
-            var starsMaterial = new THREE.PointsMaterial( { color: 'green', size: 3.0 } );
-            var geometry = new THREE.Geometry();
-
-            for(var i = 0; i < resultPoints.length; i++)
-            {
-                geometry.vertices.push(new THREE.Vector3( resultPoints[i].x, resultPoints[i].y, resultPoints[i].z ));
-            }
-
-            result = new THREE.Points( geometry, starsMaterial );
-
-            scene.add(result);
-        });
-
-        var axisHelper = new THREE.AxisHelper( 500 );
-        scene.add( axisHelper );
-
-        let controls = new THREE.OrbitControls( camera, canvas );
-        controls.target = new THREE.Vector3( 50, 50, 50);
-        controls.maxAzimuthAngle = Infinity;
-
-        function loop() {
-
+        function render() {
             renderer.render(scene, camera);
-            requestAnimationFrame(loop);
         }
 
-        loop();
+        render();
     }
 }
 
