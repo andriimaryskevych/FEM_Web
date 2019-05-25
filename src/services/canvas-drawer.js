810dff453ac84bfe5a18c866da2c9c27f7a0b69f
Andrii Maryskevych <andrii.maryskecych@gmail.com>
2019-05-25 11:33:30 +0300

    Fix white wall

--- src/services/canvas-drawer.js
+++ src/services/canvas-drawer.js
@@ -24,25 +24,6 @@ class CanvasDrawer {
 
         scene.add(new THREE.AmbientLight(0xf0f0f0));
 
-        var light = new THREE.SpotLight(0xffffff, 1.5);
-        light.position.set(0, 1500, 200);
-        light.castShadow = true;
-        light.shadow = new THREE.LightShadow(new THREE.PerspectiveCamera(70, 1, 200, 2000));
-        light.shadow.bias = -0.000222;
-        light.shadow.mapSize.width = 1024;
-        light.shadow.mapSize.height = 1024;
-        scene.add(light);
-
-        var planeGeometry = new THREE.PlaneBufferGeometry(2000, 2000);
-        planeGeometry.rotateX(-Math.PI / 2);
-
-        var planeMaterial = new THREE.ShadowMaterial({ opacity: 0.2 });
-
-        var plane = new THREE.Mesh(planeGeometry, planeMaterial);
-        plane.position.y = -200;
-        plane.receiveShadow = true;
-        scene.add(plane);
-
         var helper = new THREE.GridHelper(2000, 100);
         helper.geometry.rotateX( Math.PI / 2 );
         helper.material.opacity = 0.25;
