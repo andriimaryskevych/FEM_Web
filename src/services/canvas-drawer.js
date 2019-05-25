81fe10dfd49b5b93b9ae97ec7649027c432ad559
Andrii Maryskevych <andrii.maryskecych@gmail.com>
2019-05-25 13:27:13 +0300

    Import three js in propper way

--- src/services/canvas-drawer.js
+++ src/services/canvas-drawer.js
@@ -1,6 +1,25 @@
-const THREE = require('three');
-window.THREE = THREE;
-require('three/examples/js/controls/OrbitControls');
+import {
+    WebGLRenderer,
+
+    Scene,
+    PerspectiveCamera,
+
+    Points,
+    Vector3,
+    Triangle,
+    Geometry,
+
+    Color,
+    PointsMaterial,
+    MeshBasicMaterial,
+
+    GridHelper,
+    AxesHelper,
+
+    Face3,
+    Mesh
+ } from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
 class CanvasDrawer {
     constructor(canvas, socket) {
@@ -10,36 +29,34 @@ class CanvasDrawer {
         canvas.setAttribute('width', width);
         canvas.setAttribute('height', height);
 
-        var scene = new THREE.Scene();
-        scene.background = new THREE.Color(0xf0f0f0);
+        var scene = new Scene();
+        scene.background = new Color(0xf0f0f0);
 
         var VIEW_ANGLE = 45;
         var ASPECT = width / height;
         var NEAR = 0.1;
         var FAR = 10000;
-        let camera = new THREE.PerspectiveCamera(VIEW_ANGLE, ASPECT, NEAR, FAR);
+        let camera = new PerspectiveCamera(VIEW_ANGLE, ASPECT, NEAR, FAR);
         camera.position.z = 300;
-        camera.up = new THREE.Vector3( 0, 0, 1 );
+        camera.up = new Vector3( 0, 0, 1 );
         scene.add(camera);
 
-        scene.add(new THREE.AmbientLight(0xf0f0f0));
-
-        var helper = new THREE.GridHelper(2000, 100);
+        var helper = new GridHelper(2000, 100);
         helper.geometry.rotateX( Math.PI / 2 );
         helper.material.opacity = 0.25;
         helper.material.transparent = true;
         scene.add(helper);
 
-        var axes = new THREE.AxesHelper(1000);
+        var axes = new AxesHelper(1000);
         axes.position.set(0, 0, 0);
         scene.add(axes);
 
-        let renderer = new THREE.WebGLRenderer({ canvas });
+        let renderer = new WebGLRenderer({ canvas });
         renderer.setPixelRatio(window.devicePixelRatio);
         renderer.setClearColor(0xffffff);
 
-        let controls = new THREE.OrbitControls(camera, canvas);
-        controls.target = new THREE.Vector3( 50, 50, 0);
+        let controls = new OrbitControls(camera, canvas);
+        controls.target = new Vector3( 50, 50, 0);
         controls.update();
 
         // adding red points that represent statr poistion
@@ -61,15 +78,15 @@ class CanvasDrawer {
 
             startPoints = JSON.parse(data);
 
-            var starsMaterial = new THREE.PointsMaterial( { color: 'black', size: 2.0 } );
-            var geometry = new THREE.Geometry();
+            var starsMaterial = new PointsMaterial( { color: 'black', size: 2.0 } );
+            var geometry = new Geometry();
 
             for (let i = 0; i < startPoints['NT'].length; i++)
             {
                 let finiteElementVerticesNumbers = startPoints['NT'][i];
 
                 for (let j = 0 ; j < finiteElementVerticesNumbers.length; j++) {
-                    geometry.vertices.push(new THREE.Vector3(
+                    geometry.vertices.push(new Vector3(
                         startPoints['AKT'][finiteElementVerticesNumbers[j]][0],
                         startPoints['AKT'][finiteElementVerticesNumbers[j]][1],
                         startPoints['AKT'][finiteElementVerticesNumbers[j]][2])
@@ -77,7 +94,7 @@ class CanvasDrawer {
                 }
             }
 
-            start = new THREE.Points( geometry, starsMaterial );
+            start = new Points( geometry, starsMaterial );
 
             scene.add(start);
         });
@@ -87,8 +104,8 @@ class CanvasDrawer {
         let result;
 
         socket.on('points.txt', (data) => {
-            var starsMaterial = new THREE.PointsMaterial( { color: 'green', size: 3.0 } );
-            var geometry = new THREE.Geometry();
+            var starsMaterial = new PointsMaterial( { color: 'green', size: 3.0 } );
+            var geometry = new Geometry();
 
             resultPoints = JSON.parse(data);
 
@@ -97,7 +114,7 @@ class CanvasDrawer {
                 let finiteElementVerticesNumbers = resultPoints['NT'][i];
 
                 for (let j = 0 ; j < finiteElementVerticesNumbers.length; j++) {
-                    geometry.vertices.push(new THREE.Vector3(
+                    geometry.vertices.push(new Vector3(
                         resultPoints['AKT'][finiteElementVerticesNumbers[j]][0],
                         resultPoints['AKT'][finiteElementVerticesNumbers[j]][1],
                         resultPoints['AKT'][finiteElementVerticesNumbers[j]][2])
@@ -105,10 +122,22 @@ class CanvasDrawer {
                 }
             }
 
-            result = new THREE.Points( geometry, starsMaterial );
+            result = new Points( geometry, starsMaterial );
 
             scene.add(result);
         });
+
+        var geom = new Geometry();
+
+        geom.vertices.push(new Vector3(0, 0, 0));
+        geom.vertices.push(new Vector3(30, 0, 0));
+        geom.vertices.push(new Vector3(30, 30, 0));
+
+        geom.faces.push(new Face3(0, 1, 2));
+
+        var mesh = new Mesh(geom, new MeshBasicMaterial({color: 'gray'}));
+
+        scene.add(mesh);
 
         function loop() {
 
