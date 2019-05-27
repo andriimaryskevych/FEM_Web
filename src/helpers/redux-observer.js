70f57ce56e6aa11f373c5fa103ea86672490cda2
Andrii Maryskevych <andrii.maryskecych@gmail.com>
2019-05-27 15:15:00 +0300

    Dummy fix that should work

--- src/helpers/redux-observer.js
+++ src/helpers/redux-observer.js
@@ -2,13 +2,14 @@ const observeStore = (store, select, onChange, ignoreCount = 0) => {
     let currentState = null;
 
     const handleChange = () => {
+        let nextState = select(store.getState());
+
         if (ignoreCount) {
             ignoreCount--;
+            currentState = nextState;
 
             return;
         }
-
-        let nextState = select(store.getState());
 
         if (nextState !== currentState) {
             currentState = nextState;
