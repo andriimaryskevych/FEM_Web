5d859ff95352a57352dae20af9a9446ab349ebb3
Andrii Maryskevych <andrii.maryskecych@gmail.com>
2019-05-28 22:48:35 +0300

    Add pure component to list items

--- src/components/PressureItem.js
+++ src/components/PressureItem.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 
-export default class PressureItem extends Component {
+export default class PressureItem extends PureComponent {
     constructor (props) {
         super(props);
 
